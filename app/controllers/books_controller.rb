9b889b5c9a43a44388904bdc12f5029f5ed32f27
Kazuho Yamaguchi <kzh.yap@gmail.com>
2017-03-20 01:05:07 +0900

    Add filtering with author

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,15 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.page(params[:page])
+    @authors = Book.pluck(:author).uniq.sort
+    @books = if params[:author].present?
+      Book.where(author: params[:author]).page(params[:page])
+    else
+      Book.page(params[:page])
+    end
+  end
+
+  def search
+    index
+    render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :books, only: [:index]
+  resources :books, only: [:index] do
+    collection do
+      get :search
+    end
+  end
 end
