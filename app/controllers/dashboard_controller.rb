c6ffc6df8334e44716d16691bd8394d5149f71e1
Kazuho Yamaguchi <kzh.yap@gmail.com>
2017-03-30 19:20:45 +0900

    Display summary of tags on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
   def index
+    @tags = ActsAsTaggableOn::Tagging.where(context: :tags).includes(:tag).all.group_by(&:tag_id).map{|tag_id, group| [group.first.tag.taggings_count, group.first.tag.name] }.sort_by(&:first).reverse
   end
 end
