f1fb7a369bdd1895895afd9e56023f5212984560
Kazuho Yamaguchi <kzh.yap@gmail.com>
2017-03-30 19:22:38 +0900

    Add filter of books by tag

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,9 @@ class BooksController < ApplicationController
   def index
     @authors = Author.includes(:books).order(:name).all
     @q = Book.ransack(params[:q])
-    @books = @q.result.page(params[:page])
+    books_query = @q.result
+    books_query = books_query.tagged_with(params[:tag_name], on: params[:context]) if params[:context].present? && params[:tag_name].present?
+    @books = books_query.page(params[:page])
   end
 
   def mark_as_read
