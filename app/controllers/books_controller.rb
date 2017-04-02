935425b54eb6725c568346f78c8d915d50c53dee
Kazuho Yamaguchi <kzh.yap@gmail.com>
2017-04-02 15:35:01 +0900

    Add a filter with read flag

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def index
-    process_tag_sample_param
+    process_params
 
     @authors = Author.includes(:books).order(:name).all
     @q = Book.ransack(params[:q])
@@ -24,11 +24,24 @@ class BooksController < ApplicationController
 
   private
 
+    def process_params
+      process_tag_sample_param
+      process_read_flag_param
+    end
+
     def process_tag_sample_param
       if params[:tag_sample] == 'include'
         params[:q][:tag_eq] = 'サンプル'
       elsif params[:tag_sample] == 'exclude'
         params[:q][:tag_not_eq] = 'サンプル'
+      end
+    end
+
+    def process_read_flag_param
+      if params[:read_flag] == 'include'
+        params[:q][:read_true] = '1'
+      elsif params[:read_flag] == 'exclude'
+        params[:q][:read_blank] = '1'
       end
     end
 end
