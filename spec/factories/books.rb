d1445b1886a52c5134d914eaa38be2cd40dace23
kyamaguchi <kzh.yap@gmail.com>
2019-02-08 01:31:24 +0900

    Fix factories
    $ rubocop \
      --require rubocop-rspec \
      --only FactoryBot/AttributeDefinedStatically \
      --auto-correct

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :book do
     sequence(:asin) {|n| "B00ABCDEF#{n}" }
-    title  "Doe"
-    raw_author "Test Author"
-    date 2.months.ago.to_date
-    collection_count 0
+    title  { "Doe" }
+    raw_author { "Test Author" }
+    date { 2.months.ago.to_date }
+    collection_count { 0 }
   end
 end
