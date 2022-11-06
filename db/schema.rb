9ca4dcd6af6de481e47fc1dcdb51f7f5a878a7b2
Kazuho Yamaguchi <kzh.yap@gmail.com>
2022-11-06 19:31:18 +0900

    Update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,36 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170416165133) do
+ActiveRecord::Schema.define(version: 2017_04_16_165133) do
 
   create_table "authors", force: :cascade do |t|
-    t.string   "name"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_authors_on_name"
   end
 
   create_table "books", force: :cascade do |t|
-    t.string   "asin"
-    t.string   "title"
-    t.string   "tag"
-    t.string   "raw_author"
-    t.date     "date"
-    t.integer  "collection_count"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.integer  "author_id"
+    t.string "asin"
+    t.string "title"
+    t.string "tag"
+    t.string "raw_author"
+    t.date "date"
+    t.integer "collection_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "author_id"
     t.index ["asin"], name: "index_books_on_asin", unique: true
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
   create_table "taggings", force: :cascade do |t|
-    t.integer  "tag_id"
-    t.string   "taggable_type"
-    t.integer  "taggable_id"
-    t.string   "tagger_type"
-    t.integer  "tagger_id"
-    t.string   "context",       limit: 128
+    t.integer "tag_id"
+    t.string "taggable_type"
+    t.integer "taggable_id"
+    t.string "tagger_type"
+    t.integer "tagger_id"
+    t.string "context", limit: 128
     t.datetime "created_at"
     t.index ["context"], name: "index_taggings_on_context"
     t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 20170416165133) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string  "name"
+    t.string "name"
     t.integer "taggings_count", default: 0
     t.index ["name"], name: "index_tags_on_name", unique: true
   end
